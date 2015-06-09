15c2985b2908f8b80875372d3a9e286c4872e30b
James Nord <jnord+github@cloudbees.com>
2015-06-09 11:10:20 +0100

    Test case for [JENKINS-28799]

--- src/test/java/CompatibilityTest.java
+++ src/test/java/CompatibilityTest.java
@@ -1,9 +1,11 @@
 import junit.framework.TestResult;
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.tools.ant.AntClassLoader;
 import org.jenkinsci.bytecode.Transformer;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -31,6 +33,32 @@ public class CompatibilityTest {
         assertFieldType(i, String[].class, "y");
         assertFieldType(i, Object.class, "z");
     }
+    
+    @Test
+    public void testClassesAreOnlyReWrittenWhenNeeded() throws Exception {
+        System.out.println("rewrtting tests");
+        final Transformer t = new Transformer();
+        AntClassLoader cl = new AntClassLoader() {
+            @Override
+            protected Class<?> defineClassFromData(File container, byte[] classData, String classname) throws IOException {
+                byte[] rewritten = t.transform(classname, classData);
+                if (rewritten!=classData ) {
+                    fail(classname + " was rewritten without need");
+                }
+                return super.defineClassFromData(container, rewritten, classname);
+            }
+        };
+
+        cl.addPathComponent(new File("target/test-classes/v2"));
+        cl.addPathComponent(new File("target/test-classes/client"));
+        cl.addPathComponent(new File("target/lib/ivy.jar"));
+
+        t.loadRules(cl);
+
+        cl.loadClass("org.apache.ivy.core.settings.IvySettings");
+        System.out.println("rewrtting tests - done");
+    }
+
 
     private void assertFieldType(Class<?> i, Class<?> type, String name) throws NoSuchFieldException {
         assertSame(type, i.getDeclaredField(name).getType());
@@ -69,4 +97,5 @@ public class CompatibilityTest {
         if (tr.errorCount() + tr.failureCount()>0)
             throw new Error("test failures");
     }
+
 }
